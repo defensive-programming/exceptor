a1167cb4581ff0a982842e3d99dfa9c8fa958278
hungting li <hungtington.dev@gmail.com>
2024-07-12 08:38:50 +0800

    feat: introduce Pivot and pivot

--- src/lib.ts
+++ src/lib.ts
@@ -1,11 +1,3 @@
-// @ts-ignore: bypass TS error cuz it seems like even I can find the corresponding type definition, npm build will still fail.
-import StackTrace from 'npm:stacktrace-js';
-// @ts-ignore: bypass TS error cuz it seems like prepare-stack-trace doesn't have the corresponding type definition. without this, npm build will fail.
-import prepareStackTrace from 'npm:prepare-stack-trace';
-// @ts-ignore: bypass TS error
-import consola from "npm:consola";
-// @ts-ignore: bypass TS error
-import Axe from 'npm:axe';
 import { z as $z } from "https://deno.land/x/zod/mod.ts";
 
 // FIXME: tree-shaking might work using esm.sh, but the built npm module will have problem to be used.
@@ -21,70 +13,29 @@ export const code = {
   unauthenticated: 'unauthenticated',
 }
 
-export const logger = (config: { environment: string }) =>
-{
-  const log = new Axe({ // Options
-    // logger: consola,
-    logger: console,
-    appInfo: false,
-    level: config.environment === 'development' ? 'trace' : 'error',
-    levels: ['info','warn','error','fatal', 'debug'],
-    meta: {
-      show: true
-    },
-    silent: false
-  })
-  /**
-   * HACK:
-   * For `time` & `timeEnd`, this is inspired by another logger called "Signal".
-   * Since both Axe & consola don't have that API, we built this up anyway.
-   * For `debug`, if we just use `log.debug` without below configuration,
-   * we "might" encounter a problem when log some objects.
-   */
-  log.time = console.time
-  log.timeEnd = console.timeEnd // @ts-ignore: bypass TS error just for quick dev
-  log.debug = e => consola.debug(e)
-   // HACK: the built-in `log.info` has an issue of mutation.
-   // `log.info(a, b)` will cause `b` having some metadata that related to axe.
-   // @ts-ignore: bypass TS error just for quick dev
-  log.info = (a, b) => b === undefined ? consola.info(a) : consola.info(a, b)
-  /**
-   * Doesn't support to log non-Error things, such as a normal string.
-   * So `messageOrErrObj` can be string, provided `errObj` is an `Error`,
-   * or only provides `messageOrErrObj` as an `Error`
-   */
-  log.err = (messageOrErrObj: Error | string, errObj?: Error) => StackTrace.fromError(errObj || messageOrErrObj as Error).then(stackframes =>
+/**
+ * This is not expected to use directly.
+ * Currently, the only consumer for this is `pivot`.
+ */
+class Pivot extends Error {
+  code: string;
+  details: unknown;
+  name: string;
+  constructor(message: string = '', payload: unknown = null)
   {
-    const message = typeof messageOrErrObj === 'string' ? messageOrErrObj : messageOrErrObj.message
-    const messageTitle = `Error Message: ${message}`
-    const error = typeof messageOrErrObj === 'string' ? errObj : messageOrErrObj
-
-    console.group(messageTitle);
-    (error as Error).stack = prepareStackTrace(error, stackframes)
-    log.error(error)
-    console.groupEnd();
-  })
-  log.throw = (messageOrErrObj: Error | string, payload={}) =>
-  {
-    const $er = typeof messageOrErrObj === 'string' ? new Exception(messageOrErrObj, payload) : messageOrErrObj
-    throw $er
+    super();
+    this.name = 'Pivot';
+    this.code = message;
+    this.details = payload;
   }
-  // WARN: enable this can create an error message in background script
-  // async function hook(err, message, meta)
-  // {
-  //   // return early if we wish to ignore this
-  //   // (this prevents recursion; see end of this fn)
-  //   if (meta.ignore_hook) return;
-  //   try {
-  //     utils.api('log', { severity: meta.level, message })
-  //   } catch (err) {
-  //     log.fatal(err, { ignore_hook: true });
-  //   }
-  // }
-
-  // ['info', 'warn'].map(e => log.post(e, hook))
-  return log
-};
+}
+/**
+ * The only reason you use `pivot` is cuz you want to handle the task later.
+ * The task is currently encoded using `code` as shown below,
+ * and it'd be delegated to the closest, the same promise chain's `catch` handler.
+ * So once the `catch` got that, it can do the details work for that specific task.
+ */
+export const pivot = (code: string, payload: unknown) => { throw new Pivot(code, payload) }
 
 /**
  * ### Usage
@@ -147,7 +98,7 @@ export const Failure = Exception;
  * Otherwise, `throw new Exception(p1, p2)`
  */
 // deno-lint-ignore no-explicit-any
-export function bubble (p1: any, p2?: Record<string, unknown>)
+export function bubble (p1: any, p2?: Record<string, unknown>): never
 {
   const $e = arguments.length === 1 ? p1 : new Exception(p1, p2)
   runBeforeBubble($e)
